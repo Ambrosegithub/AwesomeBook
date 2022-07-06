a56ce0942c56d4fe2949ea726e9d18c2f4572719
Isigba Ambrose <ambrisexyz@gmail.com>
2022-07-06 12:52:35 +0200

    add class, method to add books to storage

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
+/*
 let bookList = [];
-
 function displayBooks() {
   const outputFiled = document.getElementById('displayField');
   outputFiled.innerHTML = '';
@@ -48,3 +48,75 @@ window.addEventListener('load', () => {
 
   displayBooks();
 });
+*/
+// class book list
+// class ui
+// class delete book
+// class event
+//class intances
+// validate
+// clearfield
+//event listening for delete
+// rep=> book
+
+// create a class contructor
+class bookBank {
+  constructor(title, author) {
+    this.title = title;
+    this.author = author;
+  }
+}
+// add books to store
+class bookStore {
+  // add method to get book from store
+  static getBookBank() {
+    // check if local storage is empty
+    var books;
+    if (localStorage.getItem("book" === null)) {
+      books = [];
+    } else {
+      // otherwise get books from store
+      books.JSON.parse(localStorage.getItem("books"));
+    }
+    return books;
+  }
+  // create method to add book to storage
+  static addBook(book) {
+    const books = bookStore.getBookBank();
+    books.push(book);
+    localStorage.setItem("books", JSON.stringify(books));
+  }
+  // remove book from storage
+  static removeBook(title) {
+    const books = bookStore.getBookBank();
+    //looping through each book
+    books.forEach((book, index) => {
+      // checking if target exist
+      if (book.title === title) {
+        // then remove it, if it does exist
+        books.splice(index, 1);
+      }
+    });
+    localStorage.setItem("books", JSON.stringify(books));
+  }
+} 
+// add book to ui
+class userIterface {
+  static displayBooks() {
+     const outputField = document.querySelector('displayField');
+     outputField.innerHTML = '',
+     bookList.forEach(book => {
+     const outputField = document.getElementById('displayField');
+     const bookListDiv = document.createElement('div');
+     const bookName = document.createElement('h2');
+     const bookAuthor = document.createElement('h3');
+     const removeBtn = document.createElement('button');
+     removeBtn.innerText = 'remove book';
+     const line = document.createElement('hr');
+     removeBtn.classList.add('delete');
+     bookName.innerHTML = `${book.titleName}`;
+     bookAuthor.innerHTML = `${book.authorName}`;
+ 
+   });
+  };
+ }
