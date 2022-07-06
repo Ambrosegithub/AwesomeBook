4de381caad6e89bd4ea097c73c068b4d7e96ef64
Isigba Ambrose <ambrisexyz@gmail.com>
2022-07-06 22:08:29 +0200

    modify readme file

--- webapp.js
+++ webapp.js
@@ -1,14 +1,14 @@
-const navHeader = document.querySelector('#navlink-header');
-const navcont = document.createElement('div');
-const navbar = document.createElement('nav');
-navbar.id = 'navbar';
-const navspan = document.createElement('span');
-navspan.innerText = 'awesome books';
-navspan.id = 'navspan';
-const navlist = document.createElement('ul');
-navlist.id = 'navlist';
-const navlistitem = document.createElement('li');
-const navlistLinks = document.createElement('a');
+const navHeader = document.querySelector("#navlink-header");
+const navcont = document.createElement("div");
+const navbar = document.createElement("nav");
+navbar.id = "navbar";
+const navspan = document.createElement("span");
+navspan.innerText = "awesome books";
+navspan.id = "navspan";
+const navlist = document.createElement("ul");
+navlist.id = "navlist";
+const navlistitem = document.createElement("li");
+const navlistLinks = document.createElement("a");
 navHeader.appendChild(navcont);
 navcont.appendChild(navbar);
 navbar.append(navspan, navlist);
@@ -17,37 +17,33 @@ navlistitem.append(navlistLinks);
 navlistitem.innerHTML = `<a href="#" id='link1' class="link">list</a>`;
 navlistitem.innerHTML += `<a href="#" id='link2' class="link">add new</a>`;
 navlistitem.innerHTML += `<a href="#" id='link3'class="link">contact</a>`;
+const link1 = document.getElementById("link1");
+const link2 = document.getElementById("link2");
+const link3 = document.getElementById("link3");
 
-const link1 = document.getElementById('link1');
-const link2 = document.getElementById('link2');
-const link3 = document.getElementById('link3');
-
-link1.addEventListener('click', () => {
-  const toppart = document.getElementById('top-part');
-  const form = document.getElementById('form');
-  const contactinfo = document.getElementById('contact');
-
-  toppart.style.display = 'flex';
-  form.style.display = 'none';
-  contactinfo.style.display = 'none';
+link1.addEventListener("click", () => {
+  const toppart = document.getElementById("top-part");
+  const form = document.getElementById("form");
+  const contactinfo = document.getElementById("contact");
+  toppart.style.display = "flex";
+  form.style.display = "none";
+  contactinfo.style.display = "none";
 });
 
-link2.addEventListener('click', () => {
-  const toppart = document.getElementById('top-part');
-  const form = document.getElementById('form');
-  const contactinfo = document.getElementById('contact');
-
-  toppart.style.display = 'none';
-  form.style.display = 'flex';
-  contactinfo.style.display = 'none';
+link2.addEventListener("click", () => {
+  const toppart = document.getElementById("top-part");
+  const form = document.getElementById("form");
+  const contactinfo = document.getElementById("contact");
+  toppart.style.display = "none";
+  form.style.display = "flex";
+  contactinfo.style.display = "none";
 });
 
-link3.addEventListener('click', () => {
-  const toppart = document.getElementById('top-part');
-  const form = document.getElementById('form');
-  const contactinfo = document.getElementById('contact');
-
-  toppart.style.display = 'none';
-  form.style.display = 'none';
-  contactinfo.style.display = 'flex';
+link3.addEventListener("click", () => {
+  const toppart = document.getElementById("top-part");
+  const form = document.getElementById("form");
+  const contactinfo = document.getElementById("contact");
+  toppart.style.display = "none";
+  form.style.display = "none";
+  contactinfo.style.display = "flex";
 });
